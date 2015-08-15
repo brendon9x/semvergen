5e89cd3746be0a0bda234e1009ada36bf06cf30f
Brendon McLean <brendon@intellectionsoftware.com>
2015-08-15 14:28:56 +0200

    Fix hysterical case of added specs breaking actual working code

--- lib/semvergen/change_log_file.rb
+++ lib/semvergen/change_log_file.rb
@@ -7,9 +7,9 @@ module Semvergen
     end
 
     def <<(message)
-      current_change_log = File.exist?(change_log_file) ? File.read(change_log_file) : ""
+      current_change_log = File.exist?(@change_log_filename) ? File.read(@change_log_filename) : ""
       new_change_log     = "# Changelog\n\n#{message}" + current_change_log.gsub("# Changelog\n", "")
-      File.open(change_log_file, "w") { |f| f.write new_change_log }
+      File.open(@change_log_filename, "w") { |f| f.write new_change_log }
     end
 
     def features(version=nil)
