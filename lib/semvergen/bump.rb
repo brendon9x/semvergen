e5b288d1e91ae2230e5cfc587fbad44c76c5b97e
Brendon McLean <brendon@intellectionsoftware.com>
2015-11-04 10:26:40 +0200

    Change warning message

--- lib/semvergen/bump.rb
+++ lib/semvergen/bump.rb
@@ -28,7 +28,7 @@ module Semvergen
 
     def run!(options)
       unless @shell.current_branch == "master"
-        say color("You are not on master. It is not recommended to create releases from a branch", :red)
+        say color("You are not on master. It is not recommended to create releases from a branch unless they're maintenance releases", :red)
         newline
         return unless agree("Proceed anyway? ")
         newline
