a5e12877147b0f88a8ad73f7b6342935004a7587
Tsu-Shiuan Lin <lnxtsu003@gmail.com>
2017-01-24 09:48:03 +0200

    Enforce UTF-8 encoding on changelog file read

--- lib/semvergen/change_log_file.rb
+++ lib/semvergen/change_log_file.rb
@@ -7,7 +7,7 @@ module Semvergen
     end
 
     def <<(message)
-      current_change_log = File.exist?(@change_log_filename) ? File.read(@change_log_filename) : ""
+      current_change_log = File.exist?(@change_log_filename) ? File.read(@change_log_filename, :encoding => "UTF-8") : ""
       new_change_log     = "# Changelog\n\n#{message}" + current_change_log.gsub("# Changelog\n", "")
       File.open(@change_log_filename, "w") { |f| f.write new_change_log }
     end
