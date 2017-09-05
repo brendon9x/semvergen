fc14e26dfdea3d4737794b6f049d95c6870e3e3f
db579 <baarkerlounger@gmail.com>
2017-09-05 20:01:37 +0100

    Fix broken specs

--- spec/semvergen/bump_spec.rb
+++ spec/semvergen/bump_spec.rb
@@ -1,8 +1,12 @@
 describe Semvergen::Bump do
   let(:semvergen) { Semvergen::Bump.new nil, nil, nil, nil, nil, nil, nil, nil }
+  PATCH = "Patch: Bug fixes, recommended for all (default)"
+  MINOR = "Minor: New features, but backwards compatible"
+  MAJOR = "Major: Breaking changes" 
+  let(:release_types) { [PATCH, MINOR, MAJOR] }
 
   describe :next_version do
-    let(:next_version) { semvergen.next_version current_version, release_type }
+    let(:next_version) { semvergen.next_version current_version, release_type, release_types }
     let(:current_version) { "3.2.19" }
 
     context "when bumping a major version" do
